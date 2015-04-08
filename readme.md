e6c3e65afe1cad7d697d286daa7c32e2caf2c812
Philip Pearl <phil@insidernine.com>
2015-04-08 18:59:11 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+#goslacksender
+
+There are so many go projects for Slack I couldn't choose between them, so I put together something very simple for my own purposes. This project simply allows you to post messages to a Slack channel asynchronously
---
+++ sender.go
@@ -0,0 +1,107 @@
+package goslacksender
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	// The size of the queue to the background goroutine
+	channel_size int = 100
+)
+
+// Type for queuing events to the background
+type Message struct {
+	Text      string `json:"text"`
+	Username  string `json:"username,omitempty"`
+	IconUrl   string `json:"icon_url,omitempty"`
+	IconEmoji string `json:"icon_emoji,omitempty"`
+	Channel   string `json:"channel,omitempty"`
+}
+
+type Sender interface {
+	// Queue() queues a message to be send to Slack in the background
+	Queue(msg Message)
+	// Text() sends a simple text message to the default Slack channel
+	Text(text string)
+	// Close() closes the sender and waits for any queued messages to be flushed out
+	Close()
+}
+
+type senderImpl struct {
+	url     string       // The url to post events too, including project details
+	channel chan Message // For queuing events to the background
+	done    chan bool    // For clean exiting
+}
+
+/*
+Create a new Sender.
+
+This creates a background goroutine to aggregate and send your events.
+*/
+func New(url string) Sender {
+	sender := &senderImpl{
+		url:     url,
+		channel: make(chan Message, channel_size),
+		done:    make(chan bool),
+	}
+	go sender.run()
+	return sender
+}
+
+/*
+Queue events to be sent to Slack
+*/
+func (sender *senderImpl) Queue(msg Message) {
+	sender.channel <- msg
+}
+
+// Text() sends a text message to Slack
+func (sender *senderImpl) Text(text string) {
+	sender.Queue(Message{Text: text})
+}
+
+/*
+Close the sender and wait for queued events to be sent
+*/
+func (sender *senderImpl) Close() {
+	// Closing the channel signals the background thread to exit
+	close(sender.channel)
+	// Wait for the background thread to signal it has flushed all events and exited
+	<-sender.done
+}
+
+func (sender *senderImpl) send(msg Message) {
+	// Convert data to JSON
+	buf := bytes.Buffer{}
+	encoder := json.NewEncoder(&buf)
+	err := encoder.Encode(msg)
+
+	start := time.Now()
+	rsp, err := http.Post(sender.url, "application/json", &buf)
+	if err != nil {
+		log.Printf("Failed to post msg to Slack.  %v\n", err)
+		return
+	}
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != http.StatusOK {
+		log.Printf("Failure return for slack msg post.  %d, %s\n", rsp.StatusCode, rsp.Status)
+	} else {
+		// TODO: remove once bedded in
+		log.Printf("slack message sent in %v\n", time.Since(start))
+	}
+}
+
+func (sender *senderImpl) run() {
+	for msg := range sender.channel {
+		sender.send(msg)
+	}
+
+	// Indicate that this thread is over
+	sender.done <- true
+	log.Printf("Slack sender exited\n")
+}
